dbccf4bdc1cdfb7dd604274d2af165a95ec59c10
Glitch (echo) <none>
2018-06-27 17:30:00 +0000

    📟🏩 Checkpoint ./server.js:893838/217

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
-// your code here!
-app.get('/', (req, res) => res.json({
-  hostname: req.hostname
-                                    }));
+
+app.get('/echo/:what', (req, res) => {
+  res.json({
+    hostname: req.hostname,
+    query: req.query,
+    params: req.params
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
