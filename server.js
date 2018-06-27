cece9e0f6e56332694d4810688323c748ca2c1fd
Glitch (jet-theory) <none>
2018-06-27 17:19:33 +0000

    ❤️👴 Checkpoint ./server.js:893838/494

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 // your code here!
-
+app.get('/', (req, res) => res.json({
+  hostname: req.hostname
+                                    }));
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
