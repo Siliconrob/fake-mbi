6cba37aed5638005e2069eac59ec22d7585b4a80
Glitch (echo) <none>
2020-02-21 04:14:34 +0000

    🦂👞 Checkpoint ./public/client.js:4054/459 ./views/index.html:4054/112

--- public/client.js
+++ public/client.js
@@ -1,9 +1,13 @@
-function randomSSN() {
+function randomSSN () {
   const digits = 9;
   const max = 9;
   const min = 0;
-  const ssn = Array.from({ length: digits }, () => { return Math.floor(Math.random() * (max - min + 1)) + min; }).join("");  
-  return ssn.replace(/(\d+)\/(\d+)\/(\d+)/, '$3$1$2');
+  const ssn = Array.from({ length: digits }, () => { return Math.floor(Math.random() * (max - min + 1)) + min; });    
+  return ssn.join('');
+}
+
+function randomDOB () {
+  new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
 }
 
 $(document).ready(() => {
