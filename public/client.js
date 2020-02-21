7251c1ecb661d0c9ef1d235e4d20179e637da7b2
Glitch (echo) <none>
2020-02-21 03:50:49 +0000

    👍🏐 Checkpoint ./views/index.html:4054/37 ./public/styles.css:4054/22 ./public/client.js:4054/555

--- public/client.js
+++ public/client.js
@@ -1,3 +1,8 @@
+
+function randomSSN(length) {
+  Array.from({length: 9}, () => { return Math.floor(Math.random() * (max - min + 1)) + min; }).join('');
+}
+
 $(function() {
   console.log('hello world :o');
   $('form').submit(function(event) {
